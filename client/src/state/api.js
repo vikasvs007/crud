c4835eed91ba68b861b79a271e0adf3866d97642
Vikas V S <vikasvs6363163@gmail.com>
2025-02-18 22:27:00 +0530

    Update api.js

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:5000/api",
+    baseUrl: process.env.REACT_APP_BASE_URL || "https://crud-backend-a70z.onrender.com",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().global?.token;
       if (token) {
